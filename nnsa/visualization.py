d7849a64a8fd38421178d8a759a5240b5524f50e
Jong Shin <jshin.m@gmail.com>
2022-06-19 05:29:20 -0400

    add geospsatial visualization

---
+++ nnsa/visualization.py
@@ -0,0 +1,35 @@
+def render_map(df, color, size, title, zoom_lat, zoom_lon, zoom=False):
+    fig = go.Figure(go.Scattergeo(
+        lat=df.Latitude,
+        lon=df.Longitude,
+        geojson={'center':{'lat':37, 'lon': 141}},
+        marker=dict(
+            color=df[color],
+            size=df[color]/size,
+            colorbar=dict(
+                ticks='outside'
+            )
+        ),
+    ))
+
+    if zoom:
+        fig.update_layout(
+        title=title+'<br>Zoomed in',
+        geo={
+            'center': {'lat':zoom_lat, 'lon': zoom_lon},
+            'projection_scale': 350
+            }
+        )
+    else:
+        fig.update_layout(
+            title=title,
+            width=1300,
+            height=700,
+            geo={
+                'showland': True,
+                'showocean': True,
+                'resolution':50,
+                'center': {'lat':zoom_lat, 'lon':zoom_lon},
+                'projection_scale': 10
+            }
+        )
