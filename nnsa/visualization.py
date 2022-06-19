57791bf89cb955d71f1ff9fa8838a01fd0d8e0af
Jong Shin <jshin.m@gmail.com>
2022-06-19 05:56:15 -0400

    add argument inference

--- nnsa/visualization.py
+++ nnsa/visualization.py
@@ -2,18 +2,39 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 def render_map(df, color, size, title, zoom_lat, zoom_lon, zoom=False):
+
+    if isinstance(color, str):
+        c = df[color]
+    elif isinstance(color, int):
+        c = df.iloc[:,color]
+    else:
+        return "None Rendered"
+
     fig = go.Figure(go.Scattergeo(
         lat=df.Latitude,
         lon=df.Longitude,
         geojson={'center':{'lat':37, 'lon': 141}},
         marker=dict(
-            color=df[color],
-            size=df[color]/size,
+            color=c,
+            size=c/size,
             colorbar=dict(
                 ticks='outside'
             )
         ),
     ))
+
+    fig.update_layout(
+        title=title,
+        width=1300,
+        height=700,
+        geo={
+            'showland': True,
+            'showocean': True,
+            'resolution':50,
+            'center': {'lat':zoom_lat, 'lon':zoom_lon},
+            'projection_scale': 10
+        }
+    )
 
     if zoom:
         fig.update_layout(
@@ -23,16 +44,5 @@ def render_map(df, color, size, title, zoom_lat, zoom_lon, zoom=False):
             'projection_scale': 350
             }
         )
-    else:
-        fig.update_layout(
-            title=title,
-            width=1300,
-            height=700,
-            geo={
-                'showland': True,
-                'showocean': True,
-                'resolution':50,
-                'center': {'lat':zoom_lat, 'lon':zoom_lon},
-                'projection_scale': 10
-            }
-        )
+
+    fig.show()
